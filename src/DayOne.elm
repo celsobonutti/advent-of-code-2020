5853ad46be3591fbfa59794a1b492638cf87bee8
Celso Bonutti Filho <celso.bonuttif@gmail.com>
2020-12-02 20:11:51 -0300

    adds findInList function

--- src/DayOne.elm
+++ src/DayOne.elm
@@ -23,19 +23,24 @@ findEntries list =
 
 findSum : List Int -> Int -> Int -> Maybe ( Int, Int )
 findSum list index element =
-    let
-        sums =
-            List.drop (index + 1) list
-                |> List.filter (\el -> el + element == 2020)
-    in
-    case sums of
-        [] ->
-            Nothing
-
-        head :: _ ->
-            Just ( element, head )
+    findInList (\el -> el + element == 2020) (List.drop (index + 1) list)
+        |> Maybe.map (\sum -> ( element, sum ))
 
 
 multiply : Maybe ( Int, Int ) -> Maybe Int
 multiply =
     Maybe.map (\( fst, snd ) -> fst * snd)
+
+
+findInList : (Int -> Bool) -> List Int -> Maybe Int
+findInList fun list =
+    case list of
+        [] ->
+            Nothing
+
+        head :: tail ->
+            if fun head then
+                Just head
+
+            else
+                findInList fun tail
